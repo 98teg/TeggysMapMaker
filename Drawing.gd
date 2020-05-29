ec4e0a9568491b3b1d9b60909d65176664155df0
98teg <teg98tom@gmail.com>
2020-05-29 16:22:04 +0200

    Implementing brushes

--- Drawing.gd
+++ Drawing.gd
@@ -1,90 +1,128 @@
 extends Control
 
-var pixels = 800
-var _previous_point = null
+var _pixels = 800
+
+var _previous_point = Vector2()
 var _image = Image.new()
-var _texture = ImageTexture.new()
-var drawing = false
+var _stroke = Image.new()
+var _brush = Image.new()
+var _color = Color.red
+
+var _image_tex = ImageTexture.new()
+var _stroke_tex = ImageTexture.new()
+
+var _brush_width
+var _brush_height
+
+var _drawing_stroke = false
 
 func _ready():
-    _image.create(pixels, pixels, false, Image.FORMAT_RGBA8)
-    _texture.create_from_image(_image)
+    _image.create(_pixels, _pixels, false, Image.FORMAT_RGBA8)
+    _image.fill(Color.white)
     
-    _image.fill(Color(1, 1, 1))
+    _image_tex.create_from_image(_image)
+    
+    _stroke.create(_pixels, _pixels, false, Image.FORMAT_RGBA8)
+    _stroke.fill(Color.transparent)
+
+    _stroke_tex.create_from_image(_stroke)
+    
+    create_brush("./brush.png")
 
 func _draw():
-    draw_texture(_texture, Vector2())
+    draw_texture(_image_tex, Vector2.ZERO)
+    draw_texture(_stroke_tex, Vector2.ZERO)
 
 func _input(event):
     if event is InputEventMouseButton:
-        var pos = event.position
-
         if event.get_button_index() == BUTTON_LEFT:
             if event.is_pressed():
-                draw_on_image(event.position)
-                drawing = true
+                start_stroke(event.position)
+                draw_stroke(event.position)
             else:
-                _previous_point = null
-                drawing = false
+                end_stroke()
     elif event is InputEventMouseMotion:
-        if drawing:
-            draw_on_image(event.position)
+        if _drawing_stroke:
+            draw_stroke(event.position)
             
-        
-func draw_on_image(_current_point : Vector2):
-    var size = 16
+func create_brush(path : String):
+    _brush.load(path)
+    _brush.lock()
+    
+    _brush_width = _brush.get_size().x
+    _brush_height = _brush.get_size().y
+            
+func start_stroke(pos : Vector2):
+    _stroke.lock()
+    draw_stroke_point(pos)
+    
+    _previous_point = pos
+    _drawing_stroke = true
 
-    if _previous_point == null:
-        _previous_point = _current_point
-
+func draw_stroke(_current_point : Vector2):
     var x_diff = _current_point.x - _previous_point.x
     var y_diff = _current_point.y - _previous_point.y
-    
+
     if x_diff == 0 && y_diff == 0:
-        _image.lock()
-        for i in range(_current_point.x - size, _current_point.x + size):
-            for j in range(_current_point.y - size, _current_point.y + size):
-                _image.set_pixel(i, j, Color(1, 0, 0))
-        _image.unlock()
+        draw_stroke_point(_current_point)
 
     elif abs(x_diff) > abs(y_diff):
         var a = y_diff / float(x_diff)
         var b = - a*_previous_point.x + _previous_point.y
         
-        var step
-        if _previous_point.x < _current_point.x:
-            step = 1
-        else:
-            step = -1
+        var step = 1 if _previous_point.x < _current_point.x else -1
         
         for x in range(_previous_point.x, _current_point.x, step):
-            var pos = Vector2(x, round(a*x + b))
-
-            _image.lock()
-            for i in range(pos.x - size, pos.x + size):
-                for j in range(pos.y - size, pos.y + size):
-                    _image.set_pixel(i, j, Color(1, 0, 0))
-            _image.unlock()
+            draw_stroke_point(Vector2(x, round(a*x + b)))
             
     else:
-        var a = x_diff / y_diff
+        var a = x_diff / float(y_diff)
         var b = - a*_previous_point.y + _previous_point.x
         
-        var step
-        if _previous_point.y < _current_point.y:
-            step = 1
-        else:
-            step = -1
+        var step = 1 if _previous_point.y < _current_point.y else -1
         
         for y in range(_previous_point.y, _current_point.y, step):
-            var pos = Vector2(round(a*y + b), y)
+            draw_stroke_point(Vector2(round(a*y + b), y))
 
-            _image.lock()
-            for i in range(pos.x - size, pos.x + size):
-                for j in range(pos.y - size, pos.y + size):
-                    _image.set_pixel(i, j, Color(1, 0, 0))
-            _image.unlock()
-    
     _previous_point = _current_point
-    _texture.set_data(_image)
+    update_stroke()
     update()
+    
+func draw_stroke_point(_pos : Vector2):
+    var init_x = _pos.x - _brush_width / 2
+    var init_y = _pos.y - _brush_height / 2
+    
+    var end_x = init_x + _brush_width
+    var end_y = init_y + _brush_height
+    
+    init_x = init_x if not init_x < 0 else 0
+    init_y = init_y if not init_y < 0 else 0
+    
+    end_x = end_x if not end_x >= _pixels else _pixels - 1
+    end_y = end_y if not end_y >= _pixels else _pixels - 1
+
+    for x in range(init_x, end_x):
+        for y in range(init_y, end_y):
+            var dst_alpha = _stroke.get_pixel(x, y).a
+            var src_alpha = _brush.get_pixel(x - init_x, y - init_y).a
+                    
+            var color = _color
+            color.a = dst_alpha if dst_alpha > src_alpha else src_alpha
+                        
+            _stroke.set_pixel(x, y, color)
+            
+func end_stroke():
+    _image.blend_rect(_stroke, Rect2(Vector2.ZERO,_stroke.get_size()), Vector2.ZERO)
+    update_image()
+    _stroke.fill(Color.transparent)
+    update_stroke()
+
+    update()
+
+    _drawing_stroke = false
+    
+func update_image():
+    _image_tex.set_data(_image)
+    
+func update_stroke():
+    _stroke_tex.set_data(_stroke)
