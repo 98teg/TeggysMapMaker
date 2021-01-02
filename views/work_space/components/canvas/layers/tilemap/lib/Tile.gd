1654e914174720f532d37f27868fc06b48684218
Teggy <teg98tom@gmail.com>
2021-01-02 16:14:40 +0100

    Implement TileStructure and redo Tile

--- views/work_space/components/canvas/layers/tilemap/lib/Tile.gd
+++ views/work_space/components/canvas/layers/tilemap/lib/Tile.gd
@@ -1,10 +1,9 @@
-class_name Tile
+class_name TMM_Tile
 
 
 enum SpecialTile {
 	AIR = -1,
-	OUT_OF_BOUNDS = -2,
-	UNSELECTED = -3
+	OUT_OF_BOUNDS = -2
 }
 
 
@@ -15,144 +14,12 @@ enum ConnectionType {
 }
 
 
-var _id := 0
-var _name := ""
-var _icon : Image
-var _structure_size := [1, 1]
-var _structure_colission_mask := []
-var _structure_main_tile := []
-var _layer := 0
-var _extra_tools := []
-var _subtiles := []
-var _images := {}
-var _n_states := 0
-var _connections_for_each_state := {}
-var _connection_type : int = ConnectionType.ISOLATED
-var _connected_group := 0
-var _can_connect_to_borders := true
+var tile_structure_id := 0
+var tile_relative_pos := [0, 0] setget set_tile_relative_pos
+var image := Image.new()
 
 
-func init(tile_conf: Dictionary):
-	_id = tile_conf.ID
-	_name = tile_conf.Name
-	_icon = tile_conf.Icon
-	_structure_size = tile_conf.Structure.Size
-	_structure_colission_mask = tile_conf.Structure.ColissionMask
-	_structure_main_tile = tile_conf.Structure.MainTile
-	_layer = tile_conf.Layer
-	_extra_tools = tile_conf.ExtraTools
-	_connection_type = tile_conf.ConnectionType
-	_connected_group = tile_conf.ConnectedGroup
-	_can_connect_to_borders = tile_conf.CanConnectToBorders
+func set_tile_relative_pos(new_tile_relative_pos: Array):
+	assert(new_tile_relative_pos.size() == 2)
 
-	_add_state(tile_conf.Image)
-
-	for variation in tile_conf.Variations:
-		_add_state(variation.Image, variation.Connections)
-
-	_create_subtiles()
-
-
-func get_id() -> int:
-	return _id
-
-
-func get_name() -> String:
-	return _name
-
-
-func get_icon() -> Image:
-	return _icon
-
-
-func get_structure_size() -> Array:
-	return _structure_size
-
-
-func get_layer() -> int:
-	return _layer
-
-
-func get_extra_tools() -> Array:
-	return _extra_tools
-
-
-func get_state(connection: int) -> int:
-	if _connections_for_each_state.has(connection):
-		return _connections_for_each_state[connection]
-	else:
-		return 0
-
-
-func get_n_states() -> int:
-	return _n_states
-
-
-func get_image(state := 0, subtile := [0, 0]) -> Image:
-	return _images[[state, subtile]]
-
-
-func get_connection_type() -> int:
-	return _connection_type
-
-
-func get_subtiles(subtile_of_reference := [0, 0]) -> Array:
-	if subtile_of_reference != [0, 0]:
-		var subtiles = []
-
-		for subtile in _subtiles:
-			var new_subtile = [subtile[0] - subtile_of_reference[0],
-					subtile[1] - subtile_of_reference[1]]
-			subtiles.append(new_subtile)
-
-		return subtiles
-	return _subtiles
-
-
-func can_connect_to(another_tile: Tile) -> bool:
-	if another_tile._id == SpecialTile.AIR:
-		return false
-	elif another_tile._id == SpecialTile.OUT_OF_BOUNDS:
-		return _can_connect_to_borders
-	else:
-		if _id == another_tile._id:
-			return true
-		else:
-			var same_group = _connected_group == another_tile._connected_group
-			return same_group and _connected_group != 0
-
-
-func is_a_multi_title() -> bool:
-	return _structure_size != [1, 1]
-
-
-func _add_state(image : Image, connections : Array = []):
-	var state = _n_states
-	_n_states += 1
-	var subtile_size = image.get_size().x / _structure_size[0]
-	subtile_size = Vector2(subtile_size, subtile_size)
-
-	for i in range(_structure_size[0]):
-		for j in range(_structure_size[1]):
-			if _structure_colission_mask[_structure_size[1] - 1 - j][i]:
-				var subtile = [i - _structure_main_tile[0],
-						j - _structure_main_tile[1]]
-
-				var pos_x = i * subtile_size.x
-				var pos_y = image.get_size().y - (subtile_size.y * (j + 1))
-				var rect = Rect2(Vector2(pos_x, pos_y), subtile_size)
-
-				_images[[state, subtile]] = image.get_rect(rect)
-
-	for connection in connections:
-		_connections_for_each_state[connection] = state
-
-
-func _create_subtiles():
-	for i in range(_structure_size[0]):
-		for j in range(_structure_size[1]):
-			if _structure_colission_mask[_structure_size[1] - 1 - j][i]:
-				var subtile = [i - _structure_main_tile[0],
-						j - _structure_main_tile[1]]
-
-				_subtiles.append(subtile)
+	tile_relative_pos = new_tile_relative_pos
---
+++ views/work_space/components/canvas/layers/tilemap/lib/TileStructure.gd
@@ -0,0 +1,211 @@
+class_name TMM_TileStructure
+
+
+var id := 0
+var name := ""
+var icon := Image.new()
+var tile_size := 0 setget set_tile_size
+var layer := 0 setget set_layer
+var size := [1, 1] setget set_size
+var colission_mask := [[true]] setget set_colission_mask
+var main_tile := [0, 0] setget set_main_tile
+var extra_tools := [] setget set_extra_tools
+var connection_type : int = TMM_Tile.ConnectionType.ISOLATED setget set_connection_type
+var connected_group := 0
+var can_connect_to_borders := true
+
+var _tiles := []
+var _tiles_relative_pos := []
+var _connection_id_to_autotiling_state := {}
+
+
+func width() -> int:
+	return size[0]
+
+
+func height() -> int:
+	return size[1]
+
+
+func n_autotiling_states() -> int:
+	return _tiles.size()
+
+
+func has_multiple_tiles() -> bool:
+	return size != [1, 1]
+
+
+func get_tiles_relative_pos(tile_pos_ref := [0, 0]) -> Array:
+	assert(tile_pos_ref.size() == 2)
+	for value in tile_pos_ref:
+		assert(value is int)
+
+	if tile_pos_ref != [0, 0]:
+		var tiles_relative_pos = []
+
+		for tile_relative_pos in _tiles_relative_pos:
+			var new_tile_relative_pos = [0, 0]
+			new_tile_relative_pos[0] = tile_relative_pos[0] - tile_pos_ref[0]
+			new_tile_relative_pos[1] = tile_relative_pos[1] - tile_pos_ref[1]
+
+			tiles_relative_pos.append(new_tile_relative_pos)
+
+		return tiles_relative_pos
+	return _tiles_relative_pos
+
+
+func get_tile(connection_id: int, tile_relative_pos := [0,  0]):
+	assert(tile_relative_pos.size() == 2)
+	for i in range(2):
+		assert(tile_relative_pos[i] is int)
+		assert(tile_relative_pos[i] >= 0 - main_tile[i])
+		assert(tile_relative_pos[i] < size[i] - main_tile[i])
+
+	var autotiling_state = _get_autotiling_state(connection_id)
+	var tiles_matrix = _tiles[autotiling_state]
+	return tiles_matrix[tile_relative_pos[0]][tile_relative_pos[1]]
+
+
+func set_tile_size(new_tile_size: int) -> void:
+	assert(new_tile_size > 0)
+
+	tile_size = new_tile_size
+
+
+func set_layer(new_layer: int) -> void:
+	assert(new_layer >= 0)
+
+	layer = new_layer
+
+
+func set_size(new_size: Array) -> void:
+	assert(new_size.size() == 2)
+	for value in new_size:
+		assert(value is int)
+		assert(value > 0)
+
+	size = new_size
+
+
+func set_colission_mask(new_colission_mask: Array) -> void:
+	assert(new_colission_mask.size() == height())
+	for row in new_colission_mask:
+		assert(row is Array)
+		assert(row.size() == width())
+		for value in row:
+			assert(value is bool)
+	assert(_is_trimmed(new_colission_mask))
+
+	colission_mask = new_colission_mask
+
+
+func set_main_tile(new_main_tile: Array) -> void:
+	assert(new_main_tile.size() == 2)
+	for i in range(2):
+		assert(new_main_tile[i] is int)
+		assert(new_main_tile[i] >= 0)
+		assert(new_main_tile[i] < size[i])
+
+	main_tile = new_main_tile
+
+
+func set_extra_tools(new_extra_tools: Array) -> void:
+	for extra_tool in new_extra_tools:
+		assert(Tilemap.Tool.has(extra_tool))
+
+	extra_tools = new_extra_tools
+
+
+func set_connection_type(new_connection_type: int) -> void:
+	assert(TMM_Tile.ConnectionType.has(new_connection_type))
+
+	connection_type = new_connection_type
+
+
+func generate_tiles_relative_pos() -> void:
+	for x in range(width()):
+		for y in range(height()):
+			if colission_mask[height() - 1 - y][x]:
+				_tiles_relative_pos.append([x - main_tile[0], y - main_tile[1]])
+
+
+func add_autotiling_state(image: Image, connection_ids: Array = []):
+	var autotiling_state = n_autotiling_states()
+
+	var tiles_matrix = {}
+
+	for x in range(width()):
+		for y in range(height()):
+			if colission_mask[height() - 1 - y][x]:
+				var tile = TMM_Tile.new()
+				tile.tile_structure_id = id
+
+				var i = y - main_tile[0]
+				var j = x - main_tile[1]
+				tile.tile_relative_pos = [i, j]
+
+				var pos_x = x * tile_size
+				var pos_y = image.get_size().y - (tile_size * (y + 1))
+				var pos = Vector2(pos_x, pos_y)
+				var rect = Rect2(pos, Vector2(tile_size, tile_size))
+				tile.image = image.get_rect(rect)
+
+				if not tiles_matrix.has(i):
+					tiles_matrix[i] = {}
+
+				tiles_matrix[i][j] = tile
+
+	for connection_id in connection_ids:
+		_connection_id_to_autotiling_state[connection_id] = autotiling_state
+	
+	_tiles.append(tiles_matrix)
+
+
+func _get_autotiling_state(connection_id: int) -> int:
+	if _connection_id_to_autotiling_state.has(connection_id):
+		return _connection_id_to_autotiling_state[connection_id]
+	else:
+		return 0
+
+func _is_trimmed(new_colission_mask: Array) -> bool:
+	# Check north side
+	var at_least_one_true = false
+
+	for north_values in new_colission_mask[0]:
+		if north_values:
+			at_least_one_true = true
+
+	if not at_least_one_true:
+		return false
+
+	# Check east side
+	at_least_one_true = false
+
+	for row in new_colission_mask:
+		if row[width() - 1]:
+			at_least_one_true = true
+
+	if not at_least_one_true:
+		return false
+
+	# Check south side
+	at_least_one_true = false
+
+	for south_values in new_colission_mask[height() - 1]:
+		if south_values:
+			at_least_one_true = true
+
+	if not at_least_one_true:
+		return false
+
+	# Check west side
+	at_least_one_true = false
+
+	for row in new_colission_mask:
+		if row[0]:
+			at_least_one_true = true
+
+	if not at_least_one_true:
+		return false
+
+	return true
